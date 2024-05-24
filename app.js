b8e92cb6bf1935d72e3ec186b9bdcbaccb660d3b
Ferran <ferran.docencia@gmail.com>
2024-05-24 13:10:19 +0200

    App básica acabada

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 // Cargar los módulos
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
 // Importado las rutas
 const rutas = require('./rutas')
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
 app.use(express.static('public'))
 app.use(rutas)
 
--- rutas.js
+++ rutas.js
@@ -1,10 +1,44 @@
+// Cargar MySQL
+const mysql = require('mysql');
+
 // Cargar Express
 const express = require("express");
 // Cargar configurador de rutas
 const rutas = express.Router();
 
+const configMySQL = {
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
+}
+const connMySQL = mysql.createConnection(configMySQL)
+
 rutas.get("/", (req, res) => {
-  res.render("index", { title: "Mi Agenda" });
+  // console.log(configMySQL);
+  const select = 'SELECT * FROM agenda'
+  connMySQL.query(select, (err, resultado, fields) => {
+    if (err) throw err
+    // console.log(resultado);
+    res.render("index", { title: "Mi Agenda", datos: resultado });
+  })
+});
+
+rutas.post("/insert", (req, res) => { 
+
+  console.log(req.body);
+  const {nombre, apellido, telefono, tipo} = req.body
+  const insert = `INSERT INTO agenda (nombre, apellido, telefono, tipo) VALUES ('${nombre}', '${apellido}', '${telefono}', '${tipo}')`
+  connMySQL.query(insert, (err, resultado, fields) => {
+    if (err) throw err
+    // Después del insert volvemos a la página inicial
+    res.redirect("/");
+  })
+
+})
+
+rutas.get("/form", (req, res) => {
+  res.render("form", { title: "Formulario" });
 });
 
 module.exports = rutas;
